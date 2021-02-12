cd11c158767fa61396bfa5eb59b487f27acbf3e0
Anthony Collier <hello@acollier.dev>
2021-02-12 01:25:53 -0500

    :sparkles: Implement image upload route

--- cdn.go
+++ cdn.go
@@ -3,19 +3,46 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"log"
+	"math/rand"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Embed struct {
 	Type         string `json:"type"`
 	AuthorName   string `json:"author_name"`
 	ProviderName string `json:"provider_name"`
+}
+
+type ImageResponse struct {
+	Url     string `json:"url"`
+	Success bool   `json:"success"`
+}
+
+var key = getConfigVar("SPACES_ACCESS_KEY")
+var secret = getConfigVar("SPACES_SECRET_KEY")
+var endpoint = getConfigVar("SPACES_ENDPOINT")
+var spaceURL = getConfigVar("SPACES_URL")
+var spaceName = getConfigVar("SPACES_NAME")
+var region = getConfigVar("SPACES_REGION")
+var cdnEndpoint = getConfigVar("CDN_ENDPOINT")
+
+var s3Config = &aws.Config{
+	Credentials: credentials.NewStaticCredentials(key, secret, ""),
+	Endpoint:    aws.String(endpoint),
+	Region:      aws.String(region),
 }
 
 func getConfigVar(key string) string {
@@ -54,12 +81,8 @@ func getFileSize(size int64) string {
 	}
 }
 
-func homePage(w http.ResponseWriter, _ *http.Request) {
-	fmt.Println("Endpoint Hit: homePage")
-	_, err := fmt.Fprintf(w, "Welcome to the HomePage!")
-	if err != nil {
-		log.Fatal(err)
-	}
+func homePage(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://acollier.dev", http.StatusPermanentRedirect)
 }
 
 func getImage(w http.ResponseWriter, r *http.Request) {
@@ -68,9 +91,7 @@ func getImage(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Endpoint Hit: getImage")
 
-	s3Endpoint := getConfigVar("SPACES_ENDPOINT")
-	cdnEndpoint := getConfigVar("CDN_ENDPOINT")
-	imageURL := fmt.Sprintf("%s/%s", s3Endpoint, key)
+	imageURL := fmt.Sprintf("%s/%s", spaceURL, key)
 	oembedURL := fmt.Sprintf("%s/oembed/%s", cdnEndpoint, key)
 	res, err := http.Head(imageURL)
 	if err != nil {
@@ -82,15 +103,15 @@ func getImage(w http.ResponseWriter, r *http.Request) {
 			_, err := fmt.Fprintf(
 				w,
 				`<!DOCTYPE html>
-							<html>
-								<head>
-									<meta name="theme-color" content="#dd9323">
-									<meta property="og:title" content="%v">
-									<meta content="%s" property="og:image">
-									<link type="application/json+oembed" href="%s" />
-								</head>
-							</html>
-						`,
+					<html>
+						<head>
+							<meta name="theme-color" content="#dd9323">
+							<meta property="og:title" content="%v">
+							<meta content="%s" property="og:image">
+							<link type="application/json+oembed" href="%s" />
+						</head>
+					</html>
+				`,
 				key, imageURL, oembedURL)
 			if err != nil {
 				log.Fatal(err)
@@ -109,8 +130,7 @@ func getOGEmbed(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Endpoint Hit: getOGEmbed")
 
-	s3Endpoint := getConfigVar("SPACES_ENDPOINT")
-	imageURL := fmt.Sprintf("%s/%s", s3Endpoint, key)
+	imageURL := fmt.Sprintf("%s/%s", spaceURL, key)
 
 	res, err := http.Head(imageURL)
 	if err != nil {
@@ -156,23 +176,103 @@ func getOGEmbed(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//func uploadImage(res *http.Request)  {
-//	s3Endpoint := getConfigVar("SPACES_ENDPOINT")
-//	s3AccessKey := getConfigVar("SPACES_ACCESS_KEY")
-//	s3SecretKey := getConfigVar("SPACES_SECRET_KEY")
-//
-//
-//}
+func uploadImage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: uploadImage")
+	if r.Header.Get("Access-Token") == "" {
+		fmt.Fprintf(w, "Please provide an access token!")
+		return
+	}
+
+	if r.Header.Get("Access-Token") != getConfigVar("ACCESS_TOKEN") {
+		fmt.Fprintf(w, "Invalid access token provided!")
+		return
+	}
+
+	file, fileHeader, err := r.FormFile("image")
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, "Could not get uploaded file")
+		return
+	}
+
+	defer file.Close()
+
+	s, err := session.NewSession(s3Config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileName, err := uploadFileToS3(s, file, fileHeader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonObj := ImageResponse{fmt.Sprintf("%v/%v", cdnEndpoint, fileName), true}
+	b, err := json.Marshal(jsonObj)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func uploadFileToS3(s *session.Session, file multipart.File, fileHeader *multipart.FileHeader) (string, error) {
+	size := fileHeader.Size
+	buffer := make([]byte, size)
+	_, err := file.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	tempFileName := randSeq(7) + filepath.Ext(fileHeader.Filename)
+
+	s, err = session.NewSession(s3Config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	object := s3.PutObjectInput{
+		Bucket:               aws.String(spaceName),
+		Key:                  aws.String(tempFileName),
+		ACL:                  aws.String("public-read"),
+		Body:                 strings.NewReader(string(buffer)),
+		ContentLength:        aws.Int64(size),
+		ContentType:          aws.String(http.DetectContentType(buffer)),
+		ServerSideEncryption: aws.String("AES256"),
+	}
+
+	_, err = s3.New(s).PutObject(&object)
+	if err != nil {
+		return "", err
+	}
+
+	return tempFileName, err
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/{id}", getImage)
-	myRouter.HandleFunc("/oembed/{id}", getOGEmbed)
+	myRouter.HandleFunc("/{id}", getImage).Methods("GET")
+	myRouter.HandleFunc("/oembed/{id}", getOGEmbed).Methods("GET")
+	myRouter.HandleFunc("/upload", uploadImage).Methods("POST")
+	myRouter.HandleFunc("/", homePage).Methods("GET")
 	log.Fatal(http.ListenAndServe(":3000", myRouter))
 }
 
 func main() {
-	fmt.Println("CDN v1.0 - Copyright (c) 2021 Anthony")
+	fmt.Println("CDN v1.0 - Copyright (c) 2021 Anthony Collier")
 	handleRequests()
 }
