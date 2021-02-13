801b48f0386c91b8cf36ebe6bfb20a4c6b00e6bb
Anthony Collier <hello@acollier.dev>
2021-02-13 01:25:37 -0500

    :construction: WIP: React frontend

---
+++ client/snowpack.config.js
@@ -0,0 +1,25 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2018'
+  },
+  buildOptions: {
+    out: '../server/dist',
+    baseUrl: '/admin',
+  },
+};
